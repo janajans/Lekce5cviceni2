652761fc3fc0e7f055907fac9026ae82293a2c20
janajans <1janajansova@gmail.com>
2021-03-27 16:02:52 +0100

    přidán javascript

---
+++ index.js
@@ -0,0 +1,14 @@
+//Cvičení 2 - Vytvořte podobnou stránku s vaším vlastním tématem.
+// 1.	Vytvořte prázdnou stránku, do které vložte knihovnu dayjs.
+// 2.	Vyberte si nějakou budoucí událost, jejíž datum je pevně určeno. Za pomoci knihovny dayjs vytvořte stránku zobrazující velké ANO nebo NE případně krátkou zprávu podle toho, zda už událost nastala či nikoliv.
+// 3.	Publikuje vaši stránku na GitHub pages.
+const velikonoce = dayjs('2021-04-04');
+const dnes = dayjs();
+
+const parElm = document.querySelector('#msg');
+
+if (dnes.isAfter(velikonoce)) {
+  parElm.textContent = 'ANO, POČKEJ SI ZA ROK';
+} else {
+  parElm.textContent = 'JEŠTĚ NE, ALE UŽ BRZY!';
+}
